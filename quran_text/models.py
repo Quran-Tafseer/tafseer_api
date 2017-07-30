40d3a334b40a4359f401ab2a8f2b51f19befb766
Emad Mokhtar <emad.m.habib@gmail.com>
2017-07-30 17:06:12 +0300

    First Quran Text models

--- quran_text/models.py
+++ quran_text/models.py
@@ -1,4 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.utils.translation import ugettext_lazy as _
 from django.db import models
+
+
+class Sura(models.Model):
+    """
+    Model to hold the Quran Chapters "Sura"
+    """
+    index = models.PositiveIntegerField(primary_key=True)
+    name = models.CharField(max_length=20, unique=True, verbose_name=_('Sura'))
+
+
+class Ayah(models.Model):
+    """
+    Model to hold chapters' text ot Verse "Ayat"
+    """
+    number = models.PositiveIntegerField(verbose_name=_('Number'))
+    sura = models.ForeignKey(Sura, related_name='ayat')
+    text = models.TextField()
