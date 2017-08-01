07430abf9ecbbb72fb60b16f93a97940460879b5
Emad Mokhtar <emad.m.habib@gmail.com>
2017-08-01 20:14:45 +0300

    Script to import quran text from Tanzil text file to Dajngo

--- data_text/parse_quran_text.py
+++ data_text/parse_quran_text.py
@@ -1,4 +1,7 @@
+from quran_text.models import Ayah
+
+
 with open('data_text/quran-simple.txt') as quran_text:
     for ayah in quran_text:
-        suran_num, ayah_num, ayah_text = ayah.strip().split('|')
-        # TODO create model object and insert it
+        sura_num, ayah_num, ayah_text = ayah.strip().split('|')
+        Ayah.objects.create(number=ayah_num, sura_id=sura_num, text=ayah_text)
