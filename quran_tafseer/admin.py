ad910a6c8eec7805c28a7555001f6d9b50e5bea9
Emad Mokhtar <emad.m.habib@gmail.com>
2017-08-06 15:50:27 +0300

    Add Tafseer models to admin site

--- quran_tafseer/admin.py
+++ quran_tafseer/admin.py
@@ -3,4 +3,17 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tafseer, TafseerText
+
+
+class TafseerAdmin(admin.ModelAdmin):
+    pass
+
+
+class TafseerTextAdmin(admin.ModelAdmin):
+    list_display = ('tafseer', 'ayah', 'text')
+    ordering = ['pk']
+
+
+admin.site.register(Tafseer, TafseerAdmin)
+admin.site.register(TafseerText, TafseerTextAdmin)
