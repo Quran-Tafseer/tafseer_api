40b52fc48020cb7163f88034c1de5c2f3f43198b
Emad Mokhtar <emad.m.habib@gmail.com>
2017-08-06 15:32:13 +0300

    Tafseer models

--- quran_tafseer/models.py
+++ quran_tafseer/models.py
@@ -2,5 +2,22 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+from quran_text.models import Ayah
+
+
+class Tafseer(models.Model):
+    name = models.CharField(max_length=20, verbose_name=_('Name'))
+
+    def __str__(self):
+        return self.name
+
+
+class TafseerText(models.Model):
+    tafseer = models.ForeignKey(Tafseer)
+    ayah = models.ForeignKey(Ayah)
+    text = models.TextField(verbose_name=_('Tafseer'))
+
+    def __str__(self):
+        return self.text
