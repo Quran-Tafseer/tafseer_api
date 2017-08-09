061ab4e73efb08fda83090ab4fe1fdd75d2c5634
Emad Mokhtar <emad.m.habib@gmail.com>
2017-08-09 10:48:06 +0300

    Sura Ayat Range Endpoint

--- quran_text/urls.py
+++ quran_text/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', view=views.SuraListView.as_view()),
-    url(r'^(?P<sura_num>[0-9]+)/$', view=views.SuraAyatTextView.as_view()),
-    ]
+    url(r'^(?P<sura_num>[0-9]+)/$',
+        view=views.SuraAyatTextView.as_view()),
+    url(r'^(?P<sura_num>[0-9]+)/(?P<ayah_from_num>[0-9]+)/(?P<ayah_to_num>[0-9]+)$',
+        view=views.AyahRangeTextView.as_view()),
+]
--- quran_text/views.py
+++ quran_text/views.py
@@ -21,3 +21,14 @@ class SuraAyatTextView(generics.ListAPIView):
         # TODO return 404 if no ayat
         qs = Ayah.objects.filter(sura_id=self.kwargs['sura_num'])
         return qs
+
+
+class AyahRangeTextView(generics.ListAPIView):
+    serializer_class = AyahSerializer
+
+    def get_queryset(self):
+        from_ayah = self.kwargs['ayah_from_num']
+        to_ayah = self.kwargs['ayah_to_num']
+        sura_id = self.kwargs['sura_num']
+        qs = Ayah.objects.filter(sura_id=sura_id, number__lte=to_ayah, number__gte=from_ayah)
+        return qs
