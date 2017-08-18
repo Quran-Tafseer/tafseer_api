4204b6a37430d1db3798ee86452fadaa742cdc40
Emad Mokhtar <emad.m.habib@gmail.com>
2017-08-18 17:42:57 +0300

    Remove the range view

--- quran_text/urls.py
+++ quran_text/urls.py
@@ -4,10 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', view=views.SuraListView.as_view()),
-    url(r'^(?P<sura_num>[0-9]+)/$',
-        view=views.SuraAyatTextView.as_view()),
-    url(r'^(?P<sura_num>[0-9]+)/(?P<ayah_from_num>[0-9]+)/(?P<ayah_to_num>[0-9]+)$',
-        view=views.AyahRangeTextView.as_view()),
     url(r'^(?P<sura_num>[0-9]+)/(?P<number>[0-9]+)$',
         view=views.AyahTextView.as_view(), name='ayah-detail'),
+
 ]
--- quran_text/views.py
+++ quran_text/views.py
@@ -8,34 +8,17 @@ from .models import Sura, Ayah
 
 
 class SuraListView(generics.ListAPIView):
+    """
+    Returns a full list of quran's sura (chapters) sorted by the index
+    """
     serializer_class = SuraSerializer
     queryset = Sura.objects.all()
 
 
-class SuraAyatTextView(generics.ListAPIView):
-    serializer_class = AyahSerializer
-    lookup_field = 'sura_id'
-    lookup_url_kwargs = 'sura_num'
-
-    def get_queryset(self):
-        # TODO return 404 if no ayat
-        sura_id = self.kwargs['sura_num']
-        qs = Ayah.objects.get_sura_text(sura_id)
-        return qs
-
-
-class AyahRangeTextView(generics.ListAPIView):
-    serializer_class = AyahSerializer
-
-    def get_queryset(self):
-        from_ayah = self.kwargs['ayah_from_num']
-        to_ayah = self.kwargs['ayah_to_num']
-        sura_id = self.kwargs['sura_num']
-        qs = Ayah.objects.get_sura_ayat_range(sura_id, from_ayah, to_ayah)
-        return qs
-
-
 class AyahTextView(generics.RetrieveAPIView):
+    """
+    Returns Quran text for certain verse (Ayah) in a chapter (Sura)
+    """
     serializer_class = AyahSerializer
     lookup_field = 'number'
     lookup_url_kwargs = 'number'
