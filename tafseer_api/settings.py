06d08c5f9294c51da864d780f0fbbdfd960a266d
= <=>
2017-11-15 21:46:44 +0100

    Steup Django translationa, add Arabic and English languages to settings, and some Arabic tranlsation

--- tafseer_api/settings.py
+++ tafseer_api/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-
+from django.utils.translation import ugettext_lazy as _
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -109,7 +110,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
+
+LANGUAGES = [
+    ('en', _('English')),
+    ('ar', _('Arabic'))
+]
 
 TIME_ZONE = 'UTC'
 
--- tafseer_api/urls.py
+++ tafseer_api/urls.py
@@ -1,13 +1,13 @@
-
-
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
-    url(r'^docs/', include('docs.urls')),
     url(r'^quran/', include('quran_text.urls')),
     url(r'^tafseer/', include('quran_tafseer.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
-]
+    ]
+
+urlpatterns += i18n_patterns(url(r'^docs/', include('docs.urls')))
