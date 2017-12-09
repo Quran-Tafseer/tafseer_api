5ea3b654037a60dbd31bd6afd0611c825b7cde49
= <=>
2017-12-09 14:29:09 +0100

    Modify Quran text tests

--- quran_text/tests/test_models.py
+++ quran_text/tests/test_models.py
@@ -2,20 +2,20 @@ from django.test import TestCase
 
 from model_mommy import mommy
 
-from ..models import Ayah
+from ..models import Ayah, Sura
 
 
-class SuraModelTest(TestCase):
+class TestSuraModel(TestCase):
     def setUp(self):
-        self.sura = mommy.make('quran_text.sura', name='Al-Fateha', index=1)
-        self.ayah = mommy.make('quran_text.ayah', number=1, sura=self.sura,
+        self.sura = mommy.make(Sura, name='Al-Fateha', index=1)
+        self.ayah = mommy.make(Ayah, number=1, sura=self.sura,
                                text='بسم الله الرحمن الرحيم')
 
     def test_str(self):
         self.assertEqual('Al-Fateha', str(self.sura))
 
 
-class AyahModelTest(TestCase):
+class TestAyahModel(TestCase):
     def setUp(self):
         self.sura = mommy.make('quran_text.sura', name='Al-Fateha', index=1)
         self.ayah = mommy.make('quran_text.ayah', number=1, sura=self.sura,
@@ -36,16 +36,15 @@ class TestAyahModelManager(TestCase):
                    text='الحمدلله رب العالمين')
 
     def test_get_sura_text(self):
-        ayaht = Ayah.objects.get_sura_text(self.sura.pk)
+        ayaht = Ayah.objects.get_sura_text(1)
         self.assertIn(self.ayah, ayaht)
         self.assertEqual(2, ayaht.count())
 
     def test_get_sura_ayah(self):
-        ayah = Ayah.objects.get_sura_ayah(self.sura.pk,
-                                          self.ayah.pk)
+        ayah = Ayah.objects.get_sura_ayah(1, 1)
         self.assertIn(self.ayah, ayah)
 
-    def get_sura_ayat_range(self):
-        ayaht = Ayah.objects.get_sura_ayah(self.sura.pk, 1, 2)
+    def test_get_sura_ayat_range(self):
+        ayaht = Ayah.objects.get_sura_ayat_range(1, 1, 2)
         self.assertIn(self.ayah, ayaht)
         self.assertEqual(2, ayaht.count())
