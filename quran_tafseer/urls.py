82f0196f155622fb01978e1a48a00138bb1cf96f
= <=>
2017-12-09 22:48:06 +0100

    Add names to urls to be used in tests

--- quran_tafseer/urls.py
+++ quran_tafseer/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', view=views.TafseerView.as_view()),
+    url(r'^$', view=views.TafseerView.as_view(), name='tafseer-list'),
     url(r'^(?P<tafseer_id>[0-9]+)/(?P<sura_index>[0-9]+)/(?P<ayah_number>[0-9]+)/$',
-        view=views.AyahTafseerView.as_view()),
+        view=views.AyahTafseerView.as_view(), name='ayah-tafseer'),
 ]
