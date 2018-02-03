32cdc1a7db4a08269bfc0b74feb4fe1608044b50
= <=>
2018-02-03 15:38:39 +0100

    [TST] Update test to match the new fields added

--- quran_tafseer/tests/test_serializers.py
+++ quran_tafseer/tests/test_serializers.py
@@ -9,14 +9,19 @@ class TestTafseerTextSeriazlier(TestCase):
         self.sura = mommy.make('quran_text.sura', name='Al-Fateha', index=1)
         self.ayah = mommy.make('quran_text.ayah', number=1, sura=self.sura,
                                text='بسم الله الرحمن الرحيم')
-        self.tafseer = mommy.make('quran_tafseer.Tafseer', name='simple')
+        self.tafseer = mommy.make('quran_tafseer.Tafseer', name='simple',
+                                  language='ar', book_name='simple book',
+                                  author='random')
         self.tafseer_text = mommy.make('quran_tafseer.TafseerText',
                                        ayah=self.ayah, tafseer=self.tafseer,
                                        text='بسم الله الرحمن الرحيم')
 
     def test_tafseer_serializer(self):
         serializer = TafseerSerializer(self.tafseer)
-        self.assertEqual(serializer.data, {'id': 1, 'name': 'simple'})
+        self.assertEqual(serializer.data, {'id': 1, 'name': 'simple',
+                                           'language': 'ar',
+                                           'author': 'random',
+                                           'book_name': 'simple book'})
 
     def test_tafseer_text_serializer(self):
         serializer = TafseerTextSerializer(self.tafseer_text)
